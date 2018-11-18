dab92072b576c51263d72759ab49573ac5990666
William Doom <doomwn@gmail.com>
2018-11-17 17:43:21 -1000

    refactor: updates
    updates

---
+++ Movie/et_cent.py
@@ -0,0 +1,20 @@
+import media
+import fresh_tomatoes
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys","https://images-na.ssl-images-amazon.com/images/I/61euyU4feZL._SY445_.jpg","https://youtu.be/tN1A2mVnrOM")
+#print(toy_story.storyline)
+#toy_story.show_trailer()
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys","https://images-na.ssl-images-amazon.com/images/I/61euyU4feZL._SY445_.jpg","https://youtu.be/tN1A2mVnrOM")
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys","https://images-na.ssl-images-amazon.com/images/I/61euyU4feZL._SY445_.jpg","https://youtu.be/tN1A2mVnrOM")
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys","https://images-na.ssl-images-amazon.com/images/I/61euyU4feZL._SY445_.jpg","https://youtu.be/tN1A2mVnrOM")
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys","https://images-na.ssl-images-amazon.com/images/I/61euyU4feZL._SY445_.jpg","https://youtu.be/tN1A2mVnrOM")
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys","https://images-na.ssl-images-amazon.com/images/I/61euyU4feZL._SY445_.jpg","https://youtu.be/tN1A2mVnrOM")
+
+movies = [toy_story]
+fresh_tomatoes.open_movies_page(movies)
+print(media.Movie.VALID_RATINGS) 
